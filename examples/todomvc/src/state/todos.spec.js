f9d805cd8fd42ab709866fc96664f9917b360ca6
Bradford Lemley <bradfordlemley@gmail.com>
2019-07-25 08:34:38 -0600

    add todo li

---
+++ examples/todomvc/src/state/todos.spec.js
@@ -0,0 +1,249 @@
+import Todos from './todos'
+
+describe('todos reducer', () => {
+
+  it('should handle initial state', () => {
+    const todos = Todos();
+    expect(todos.state).toEqual([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+  })
+
+  it('should handle ADD_TODO', () => {
+    let todos = Todos([]);
+    todos.add('Run the tests');
+    expect(todos.state).toEqual([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 0
+      }
+    ])
+
+    todos = Todos([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ]);
+
+    todos.add('Run the tests');
+    expect(todos.state).toEqual([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      },
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 1
+      }
+    ])
+
+    todos.add('Fix the tests');
+    expect(todos.state).toEqual([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      },
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 1
+      },
+      {
+        text: 'Fix the tests',
+        completed: false,
+        id: 2
+      }
+    ])
+  })
+
+  it('should handle DELETE_TODO', () => {
+    const todos = Todos([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      },
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 1
+      }
+    ]);
+    todos.delete(1);
+
+    expect(todos.state)
+      .toEqual([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+  })
+
+  it('should handle EDIT_TODO', () => {
+    const todos = Todos([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 1
+      }, {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ]);
+    todos.edit(1, 'Fix the tests');
+    expect(todos.state).toEqual([
+      {
+        text: 'Fix the tests',
+        completed: false,
+        id: 1
+      }, {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+  })
+
+  it('should handle COMPLETE_TODO', () => {
+    const todos = Todos([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 1
+      }, {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ]);
+
+    todos.complete(1);
+    expect(todos.state).toEqual([
+      {
+        text: 'Run the tests',
+        completed: true,
+        id: 1
+      }, {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+  })
+
+  it('should handle COMPLETE_ALL_TODOS', () => {
+    let todos = Todos([
+        {
+          text: 'Run the tests',
+          completed: true,
+          id: 1
+        }, {
+          text: 'Use Redux',
+          completed: false,
+          id: 0
+        }
+      ]
+    );
+
+    todos.completeAll();
+    expect(todos.state).toEqual([
+      {
+        text: 'Run the tests',
+        completed: true,
+        id: 1
+      }, {
+        text: 'Use Redux',
+        completed: true,
+        id: 0
+      }
+    ])
+
+    // Unmark if all todos are currently completed
+    todos = Todos ([
+        {
+          text: 'Run the tests',
+          completed: true,
+          id: 1
+        }, {
+          text: 'Use Redux',
+          completed: true,
+          id: 0
+        }
+      ]);
+    todos.completeAll();
+    expect(todos.state).toEqual([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 1
+      }, {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+  })
+
+  it('should handle CLEAR_COMPLETED', () => {
+    const todos = Todos([
+        {
+          text: 'Run the tests',
+          completed: true,
+          id: 1
+        }, {
+          text: 'Use Redux',
+          completed: false,
+          id: 0
+        }
+      ])
+    todos.clearCompleted();
+    expect(todos.state).toEqual([
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+  })
+
+  it('should not generate duplicate ids after CLEAR_COMPLETED', () => {
+    const todos = Todos([
+      {
+        id: 0,
+        completed: false,
+        text: 'Use Redux'
+      }, {
+        id: 1,
+        completed: false,
+        text: 'Write tests'
+      }
+    ]);
+    todos.complete(0);
+    todos.clearCompleted();
+    todos.add('Write more tests');
+    expect(todos.state).toEqual([
+      {
+        text: 'Write tests',
+        completed: false,
+        id: 1
+      }, {
+        text: 'Write more tests',
+        completed: false,
+        id: 2
+      }
+    ])
+  })
+})
